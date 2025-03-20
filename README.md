926afc16ba670ebaf526a7788703a7fbf9d0f339
EbenezerDavid <2331858952zt@gmail.com>
2025-03-20 16:28:22 +0800

    Update

---
+++ README.md
@@ -0,0 +1,11 @@
+# This is a simple encrypt program.
+
+### If you are Linux or Mac
+
+![Static Badge](https://img.shields.io/badge/Language-C-green)
+
+![Static Badge](https://img.shields.io/badge/Language-Bash-blue)
+
+```bash
+
+sudo ./install.sh
--- include/encryptff.h
+++ include/encryptff.h
@@ -1,3 +1,14 @@
+/*
+This a simple encrypt file an floder's program.
+The projects by GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+Look at detail go to LICENSE file.
+
+Time: 2025/03/20        Author: EbenezerDavid
+
+*/
+
+
+
 #ifndef ENCRYPTFF_H
 #define ENCRYPTFF_H
 
